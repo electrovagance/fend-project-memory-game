d6d8790d191f269a4c33c654cbca575da4cb0e87
Malak Tayeh <malaktayeh22@gmail.com>
2018-05-26 17:51:59 -0400

    create a variable that holds all cards as an HTMLCollection

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const list = document.getElementsByClassName('card');
 
 /*
  * Display the cards on the page
@@ -36,3 +36,7 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ list.addEventListener('click', function(e){
+     console.log('click!');
+ })
